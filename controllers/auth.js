71d7a066c59bfb01c80547574de35f16fbd6fa86
Juan Anzorena <chinooanzorena@gmail.com>
2023-12-03 22:08:44 -0300

    roles

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,11 +26,13 @@ const login = async (req = request, res = response) => {
 
         //GENERAR TOKEN
         const token = await generarJWT(usuario.id);
-
+        const role = usuario.role
+        
         res.status(200).json({
             usuario,
             token,
-        });
+            role
+            });
         
     } catch (error) {
         console.log(error)
